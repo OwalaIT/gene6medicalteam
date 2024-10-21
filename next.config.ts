fd82041d1880cfcab0f377a752ca36535ff8fcc3
dareayoola4 <dareayoola4@gmail.com>
2024-10-21 19:40:33 +0000

    first commit1

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
+  // Remove or set to false if you're not using images
+  images: {
+    unoptimized: true
+  }
 }
+
+module.exports = nextConfig
